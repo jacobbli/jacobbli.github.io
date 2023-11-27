1379d3674d6cfc60300928d92c939520bb4a9ecb
Jacob Li <jacob@jacobli.me>
2023-11-26 21:19:49 -0800

    Fix icon color

--- src/components/icons/BaseIcon.vue
+++ src/components/icons/BaseIcon.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="icon__main">
+  <div class="icon__main" :class="colorThemeClass">
     <slot></slot>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, computed, inject } from "vue";
 
 defineProps({
   colorProp: {
@@ -13,6 +13,12 @@ defineProps({
     default: "",
   },
 });
+
+const isDarkMode = inject("isDarkMode");
+
+const colorThemeClass = computed(() =>
+  isDarkMode.value ? "baseIcon__darkMode" : "baseIcon__lightMode"
+);
 </script>
 
 <style lang="scss" scoped>
@@ -20,5 +26,13 @@ defineProps({
   display: flex;
   width: 20px;
   fill: v-bind(colorProp);
+}
+
+.baseIcon__lightMode {
+  fill: black;
+}
+
+.baseIcon__darkMode {
+  fill: #e6e6e6;
 }
 </style>
